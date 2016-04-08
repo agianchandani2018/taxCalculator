16b0ae80a232155db0b3e9970cbc7b4e7db8aa20
Ami <agianchandani2018@pingry.org>
2016-04-08 12:25:20 -0400

    One function. Bad 'for' loop. Working all cases

--- taxCalculator.py
+++ taxCalculator.py
@@ -2,86 +2,210 @@
 #Date: April XX
 
 import math
+# 
+# def calculate_tax(adjustedIncome):
+# 
+# 	'''Takes a users income and outputs the money they owe in taxes for 3 plans'''
+# 	adjustedIncome = grossIncome - 9500
+# 
+# 	#DONE under 2,650
+# 	if adjustedIncome <= bracket1:
+# 		totalTaxesOwed = percentTax1 * adjustedIncome
+# 		overflowIncome = 0
+# 
+# 	#DONE under 27,850
+# 	if adjustedIncome > bracket1 and adjustedIncome <= bracket2:
+# 		totalTaxesOwed = percentTax1 * bracket1
+# 		overflowIncome = adjustedIncome - bracket1
+# 		totalTaxesOwed += overflowIncome * percentTax2
+# 
+# 	#DONE under 59,900	
+# 	if adjustedIncome > bracket2 and adjustedIncome <= bracket3:
+# 		totalTaxesOwed = percentTax1 * bracket1	
+# 		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
+# 		overflowIncome = adjustedIncome - bracket2
+# 		totalTaxesOwed += percentTax3 * overflowIncome
+# 	
+# 
+# 	#DONE under 134,200
+# 	if adjustedIncome > bracket3 and adjustedIncome <= bracket4:
+# 		totalTaxesOwed = percentTax1 * bracket1
+# 		overflowIncome = adjustedIncome - bracket1
+# 		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
+# 		overflowIncome = adjustedIncomee - bracket2
+# 		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
+# 		overflowIncome = adjustedIncome - bracket3
+# 		totalTaxesOwed += percentTax4 * overflowIncome
+# 
+# 	#DONE under 289950
+# 	if adjustedIncome > bracket4 and adjustedIncome <= bracket5:
+# 		totalTaxesOwed = percentTax1 * bracket1
+# 		overflowIncome = adjustedIncome - bracket1
+# 		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
+# 		overflowIncome = adjustedIncome - bracket2
+# 		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
+# 		overflowIncome = adjustedIncome - bracket3	
+# 		totalTaxesOwed += percentTax4 * (bracket4 - bracket3)
+# 		overflowIncome = adjustedIncome - bracket4
+# 		totalTaxesOwed = percentTax5 * overflowIncome
+# 
+# 	#DONE under infinity
+# 	if adjustedIncome > bracket5:
+# 		totalTaxesOwed = percentTax1 * bracket1
+# 		overflowIncome = adjustedIncome - bracket1
+# 		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
+# 		overflowIncome = adjustedIncome - bracket2
+# 		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
+# 		overflowIncome = adjustedIncome - bracket3	
+# 		totalTaxesOwed += percentTax4 * (bracket4 - bracket3)
+# 		overflowIncome = adjustedIncome - bracket4
+# 		totalTaxesOwed += percentTax5 *(bracket5 - bracket4)
+# 		overflowIncome = adjustedIncome - bracket5
+# 		totalTaxesOwed += percentTax6 * overflowIncome
+# 
+# 	return float(totalTaxesOwed)
 
-def calculate_tax(adjustedIncome):
 
-	'''Takes a users income and outputs the money they owe in taxes for 3 plans'''
-	adjustedIncome = grossIncome - 9500
 
-	#DONE under 2,650
-	if adjustedIncome <= bracket1:
-		totalTaxesOwed = percentTax1 * adjustedIncome
-		overflowIncome = 0
+#-----------------------------Bracket list attempt----------------------------------------
 
-	#DONE under 27,850
-	if adjustedIncome > bracket1 and adjustedIncome <= bracket2:
-		totalTaxesOwed = percentTax1 * bracket1
-		overflowIncome = adjustedIncome - bracket1
-		totalTaxesOwed += overflowIncome * percentTax2
 
-	#DONE under 59,900	
-	if adjustedIncome > bracket2 and adjustedIncome <= bracket3:
-		totalTaxesOwed = percentTax1 * bracket1	
-		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
-		overflowIncome = adjustedIncome - bracket2
-		totalTaxesOwed += percentTax3 * overflowIncome
+bracket1 = [2650, 8025, 8700]
+bracket2 = [27850, 32550, 35350]
+bracket3 = [59900, 78850, 85650]
+bracket4 = [134200, 164550, 178650]
+bracket5 = [289950, 357700, 388350]
+#bracket6 = [math.inf, math.inf, math.inf]
+
+percentTax1 = [0, .1, .1]
+percentTax2 = [.15, .15, .15]
+percentTax3 = [.28, .25, .25]
+percentTax4 = [.31, .28, .28]
+percentTax5 = [.36, .33, .33]
+percentTax6 = [.396, .35, .35]
+
+
+# plan2000 = [2650, 27850, 59900, 134200, 289950]
+# plan2008 = [8025, 32550, 78850, 164550, 357700]
+# plan2014 = [8700, 35350, 85650, 178650, 388350]
+# 
+# percent2000 = [0, .15, .28, .31, .36, .396]
+# percent2008 = [.1, .15, .25, .28, .33, .35]
+# percent2014 = [.1, .15, .25, .28, .33, .35]
+
+def calculate_tax(adjustedIncome, i):
+
+		'''Takes a users income and outputs the money they owe in taxes for 3 plans'''
+
+#NEED TO EDIT THIS LINE EVERY TIME THE LIST CHANGES	
+	#for i in range(3):
+	
+		if grossIncome >= 9500:
+			adjustedIncome = grossIncome - 9500
+		else:
+			adjustedIncome = grossIncome - 0	
+
+		#DONE under 2,650
+		if adjustedIncome <= bracket1[i]:
+			totalTaxesOwed = percentTax1[i] * adjustedIncome
+			overflowIncome = 0
+
+		#DONE under 27,850
+		if adjustedIncome > bracket1[i] and adjustedIncome <= bracket2[i]:
+			totalTaxesOwed = percentTax1[i] * bracket1[i]
+			overflowIncome = adjustedIncome - bracket1[i]
+			totalTaxesOwed += overflowIncome * percentTax2[i]
+
+		#DONE under 59,900	
+		if adjustedIncome > bracket2[i] and adjustedIncome <= bracket3[i]:
+			totalTaxesOwed = percentTax1[i] * bracket1[i]	
+			totalTaxesOwed += percentTax2[i] * (bracket2[i] - bracket1[i])
+			overflowIncome = adjustedIncome - bracket2[i]
+			totalTaxesOwed += percentTax3[i] * overflowIncome
 	
 
-	#DONE under 134,200
-	if adjustedIncome > bracket3 and adjustedIncome <= bracket4:
-		totalTaxesOwed = percentTax1 * bracket1
-		overflowIncome = adjustedIncome - bracket1
-		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
-		overflowIncome = adjustedIncomee - bracket2
-		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
-		overflowIncome = adjustedIncome - bracket3
-		totalTaxesOwed += percentTax4 * overflowIncome
+		#DONE under 134,200
+		if adjustedIncome > bracket3[i] and adjustedIncome <= bracket4[i]:
+			totalTaxesOwed = percentTax1[i] * bracket1[i]
+			overflowIncome = adjustedIncome - bracket1[i]
+			totalTaxesOwed += percentTax2[i] * (bracket2[i] - bracket1[i])
+			overflowIncome = adjustedIncome - bracket2[i]
+			totalTaxesOwed += percentTax3[i] * (bracket3[i] - bracket2[i])
+			overflowIncome = adjustedIncome - bracket3[i]
+			totalTaxesOwed += percentTax4[i] * overflowIncome
 
-	#DONE under 289950
-	if adjustedIncome > bracket4 and adjustedIncome <= bracket5:
-		totalTaxesOwed = percentTax1 * bracket1
-		overflowIncome = adjustedIncome - bracket1
-		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
-		overflowIncome = adjustedIncome - bracket2
-		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
-		overflowIncome = adjustedIncome - bracket3	
-		totalTaxesOwed += percentTax4 * (bracket4 - bracket3)
-		overflowIncome = adjustedIncome - bracket4
-		totalTaxesOwed = percentTax5 * overflowIncome
+		#DONE under 289950
+		if adjustedIncome > bracket4[i] and adjustedIncome <= bracket5[i]:
+			totalTaxesOwed = percentTax1[i] * bracket1[i]
+			overflowIncome = adjustedIncome - bracket1[i]
+			totalTaxesOwed += percentTax2[i] * (bracket2[i] - bracket1[i])
+			overflowIncome = adjustedIncome - bracket2[i]
+			totalTaxesOwed += percentTax3[i] * (bracket3[i] - bracket2[i])
+			overflowIncome = adjustedIncome - bracket3[i]	
+			totalTaxesOwed += percentTax4[i] * (bracket4[i] - bracket3[i])
+			overflowIncome = adjustedIncome - bracket4[i]
+			totalTaxesOwed = percentTax5[i] * overflowIncome
 
-	#DONE under infinity
-	if adjustedIncome > bracket5:
-		totalTaxesOwed = percentTax1 * bracket1
-		overflowIncome = adjustedIncome - bracket1
-		totalTaxesOwed += percentTax2 * (bracket2 - bracket1)
-		overflowIncome = adjustedIncome - bracket2
-		totalTaxesOwed += percentTax3 * (bracket3 - bracket2)
-		overflowIncome = adjustedIncome - bracket3	
-		totalTaxesOwed += percentTax4 * (bracket4 - bracket3)
-		overflowIncome = adjustedIncome - bracket4
-		totalTaxesOwed += percentTax5 *(bracket5 - bracket4)
-		overflowIncome = adjustedIncome - bracket5
-		totalTaxesOwed += percentTax6 * overflowIncome
+		#DONE under infinity
+		if adjustedIncome > bracket5[i]:
+			totalTaxesOwed = percentTax1[i] * bracket1[i]
+			overflowIncome = adjustedIncome - bracket1[i]
+			totalTaxesOwed += percentTax2[i] * (bracket2[i] - bracket1[i])
+			overflowIncome = adjustedIncome - bracket2[i]
+			totalTaxesOwed += percentTax3[i] * (bracket3[i] - bracket2[i])
+			overflowIncome = adjustedIncome - bracket3[i]
+			totalTaxesOwed += percentTax4[i] * (bracket4[i] - bracket3[i])
+			overflowIncome = adjustedIncome - bracket4[i]
+			totalTaxesOwed += percentTax5[i] *(bracket5[i] - bracket4[i])
+			overflowIncome = adjustedIncome - bracket5[i]
+			totalTaxesOwed += percentTax6[i] * overflowIncome
 
-	return float(totalTaxesOwed)
+		return float(totalTaxesOwed)
+
+
+
 
 
 #---------------------------------Main Program--------------------------------------------
 
 #2000 plan
-bracket1 = 2650
-bracket2 = 27850
-bracket3 = 59900
-bracket4 = 134200
-bracket5 = 289950
-bracket6 = math.inf
+# bracket1 = 2650
+# bracket2 = 27850
+# bracket3 = 59900
+# bracket4 = 134200
+# bracket5 = 289950
+# bracket6 = math.inf
+# 
+# percentTax1 = 0
+# percentTax2 = .15
+# percentTax3 = .28
+# percentTax4 = .31
+# percentTax5 = .36
+# percentTax6 = .396
 
-percentTax1 = 0
-percentTax2 = .15
-percentTax3 = .28
-percentTax4 = .31
-percentTax5 = .36
-percentTax6 = .396
+
+
+# bracket1 = [2650, 8025, 8700]
+# bracket2 = [27850, 32550, 35350]
+# bracket3 = [59900, 78850, 85650]
+# bracket4 = [134200, 164550, 178650]
+# bracket5 = [289950, 357700, 388350]
+# bracket6 = math.inf
+# 
+# percentTax1 = [0, .1, .1]
+# percentTax2 = [.15, .15, .15]
+# percentTax3 = [.28, .25, .25]
+# percentTax4 = [.31, .28, .28]
+# percentTax5 = [.36, .33, .33]
+# percentTax6 = [.396, .35, .35]
+
+# plan2000 = [2650, 27850, 59900, 134200, 289950]
+# plan2008 = [8025, 32550, 78850, 164550, 357700]
+# plan2014 = [8700, 35350, 85650, 178650, 388350]
+# 
+# percent2000 = [0, .15, .28, .31, .36, .396]
+# percent2008 = [.1, .15, .25, .28, .33, .35]
+# percent2014 = [.1, .15, .25, .28, .33, .35]
 
 
 
@@ -99,23 +223,39 @@ while x == 1:
 
 		adjustedIncome = float(grossIncome - 9500)
 
-		percent = round(100 * (calculate_tax(adjustedIncome)/grossIncome), 2)
+	#	percent = round(100 * (calculate_tax(adjustedIncome)/grossIncome), 2)
 
-# 		print(grossIncome)
-# 		print(adjustedIncome)
+		tax2000 = calculate_tax(grossIncome, 0)
+		tax2008 = calculate_tax(grossIncome, 1)
+		tax2014 = calculate_tax(grossIncome, 2)
+		
+		percent2000 = round(100 * (tax2000/grossIncome), 2)
+		percent2008 = round(100 * (tax2008/grossIncome), 2)
+		percent2014 = round(100 * (tax2014/grossIncome), 2)
+
+		print()
 		print("Results for the 2000 plan")
-		print("Taxes owed: ${}".format(calculate_tax(grossIncome)))
-		print("Percent of gross: {}%".format(percent))
-		print("Net income: ${}".format(grossIncome - calculate_tax(grossIncome)))
+		print("Taxes owed: ${}".format(tax2000))
+		print("Percent of gross: {}%".format(percent2000))
+		print("Net income: ${}".format(grossIncome - tax2000))
+		print()
+		
+		print("Results for the 2008 plan")
+		print("Taxes owed: ${}".format(tax2008))
+		print("Percent of gross: {}%".format(percent2008))
+		print("Net income: ${}".format(grossIncome - tax2008))
+		print()
+
+		print("Results for the 2014 plan")
+		print("Taxes owed: ${}".format(tax2014))
+		print("Percent of gross: {}%".format(percent2014))
+		print("Net income: ${}".format(grossIncome - tax2014))
+		print()
+		
 		x=0
 
 
-# grossIncome = round(grossIncome, 2)
-# totalTaxesOwed = round(totalTaxesOwed, 2)
-# netIncome = grossIncome - totalTaxesOwed
-# 
-# print("Your gross income is ${}.".format(grossIncome))
-# print("You owe ${} in taxes".format(totalTaxesOwed))
-# print("Your net income is ${}".format(netIncome))
-# 
 # print("Your efficiency overflow is {}".format(overflowIncome))
+
+
+
